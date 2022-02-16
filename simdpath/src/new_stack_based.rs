6e37516f5a284017ae9077bf74bcf6706069231f
V0ldek <mat@gienieczko.com>
2022-02-16 10:49:00 +0100

    Removed log::debug.

--- simdpath/src/new_stack_based.rs
+++ simdpath/src/new_stack_based.rs
@@ -1,5 +1,4 @@
 //! WIP version of [`stack_based`](`crate::stack_based`).
-use log;
 use std::iter::Peekable;
 
 use crate::bytes::align::{alignment, AlignedSlice};
@@ -63,7 +62,6 @@ where
             State::Initial => self.transition_based_on_node(next_node),
             State::MatchLabel(label) => {
                 if let Some(&Structural::Colon(idx)) = self.classifier.peek() {
-                    log::debug!("Colon detected, matching label.");
                     self.classifier.next();
                     let len = label.len();
                     if idx >= len + 2 {
@@ -74,59 +72,45 @@ where
 
                         let opening_quote_idx = closing_quote_idx - len - 1;
                         let slice = &self.bytes[opening_quote_idx..closing_quote_idx + 1];
-                        log::debug!("Label is: {:?}", unsafe {
-                            std::str::from_utf8_unchecked(slice)
-                        });
 
                         if slice == label.bytes_with_quotes() {
-                            log::debug!("Label matched.");
                             self.transition_based_on_node(next_node)
                         }
                     }
                 }
             }
-            State::RecursiveDescent => {
-                loop {
-                    self.transition_based_on_node(next_node);
-                    match self.classifier.peek() {
-                        Some(Structural::Opening(_)) => {
-                            self.classifier.next();
-                            log::debug!("RecursiveDescent push.");
-                            self.run_state(State::RecursiveDescent, next_node)
-                        }
-                        Some(Structural::Closing(_)) => {
-                            self.classifier.next();
-                            break;
-                        }
-                        _ => (),
+            State::RecursiveDescent => loop {
+                self.transition_based_on_node(next_node);
+                match self.classifier.peek() {
+                    Some(Structural::Opening(_)) => {
+                        self.classifier.next();
+                        self.run_state(State::RecursiveDescent, next_node)
                     }
+                    Some(Structural::Closing(_)) => {
+                        self.classifier.next();
+                        break;
+                    }
+                    _ => (),
                 }
-                log::debug!("RecursiveDescent pop.");
-            }
+            },
         }
     }
 
     fn transition_based_on_node(&mut self, node: Option<&JsonPathQueryNode>) {
         match node {
             None => {
-                log::debug!("Transitioning to Accepting.");
                 self.count += 1;
             }
             Some(JsonPathQueryNode::Root(child)) => {
                 if let Some(Structural::Opening(_)) = self.classifier.peek() {
-                    log::debug!("Transitioning to Initial.");
                     self.run_state(State::Initial, child.as_deref());
                 }
             }
             Some(JsonPathQueryNode::Label(label, child)) => {
-                log::debug!("Transitioning to MatchLabel({:?}).", unsafe {
-                    std::str::from_utf8_unchecked(label)
-                });
                 self.run_state(State::MatchLabel(label), child.as_deref())
             }
             Some(JsonPathQueryNode::Descendant(child)) => {
                 if let Some(Structural::Opening(_)) = self.classifier.peek() {
-                    log::debug!("Transitioning to RecursiveDescent.");
                     self.classifier.next();
                     self.run_state(State::RecursiveDescent, Some(child));
                 }
