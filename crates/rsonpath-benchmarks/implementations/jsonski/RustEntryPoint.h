d754bd5f60dd11c7f9ea1e2c482b6a61294d293d
Mateusz Gienieczko <mat@gienieczko.com>
2022-11-21 20:37:24 +0000

    Remove old files

--- crates/rsonpath-benchmarks/implementations/jsonski/RustEntryPoint.h
+++
@@ -1,5 +0,0 @@
-#include "Records.h"
-
-extern "C" Record *loadFile(char *file_path);
-extern "C" long runJsonSki(char *query, Record *record);
-extern "C" void dropFile(Record *record);
--- crates/rsonpath-benchmarks/src/prelude.rs
+++
@@ -1,6 +0,0 @@
-pub use crate::benchsets;
-pub use crate::dataset;
-pub use crate::framework::BenchTarget;
-pub use crate::framework::BenchmarkError;
-pub use crate::framework::Benchset;
-pub use criterion::Criterion;
