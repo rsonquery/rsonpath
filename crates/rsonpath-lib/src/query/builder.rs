fbe709fe05c49feb753a0606c1d8ea4e13433995
Mateusz Gienieczko <mat@gienieczko.com>
2023-01-22 17:42:51 +0000

    chore: fix clippy lints for query builder

--- crates/rsonpath-lib/src/query/builder.rs
+++ crates/rsonpath-lib/src/query/builder.rs
@@ -1,47 +1,71 @@
 //! Utility for building a [`JsonPathQuery`](`crate::query::JsonPathQuery`)
 //! programatically.
-//!
-//! # Examples
-//! ```
-//! # use rsonpath_lib::query::{JsonPathQuery, Label, builder::JsonPathQueryBuilder};
-//!
-//! let builder = JsonPathQueryBuilder::new()
-//!     .child(Label::new("a"))
-//!     .descendant(Label::new("b"))
-//!     .any_child()
-//!     .child(Label::new("c"));
-//!
-//! // Can also use `builder.build()`.
-//! let query: JsonPathQuery = builder.into();
-//!
-//! assert_eq!(format!("{query}"), "$['a']..['b'][*]['c']");
-//! ```
 use super::{JsonPathQuery, JsonPathQueryNode, Label};
 
+/// Builder for [`JsonPathQuery`] instances.
+///
+/// # Examples
+/// ```
+/// # use rsonpath_lib::query::{JsonPathQuery, Label, builder::JsonPathQueryBuilder};
+/// let builder = JsonPathQueryBuilder::new()
+///     .child(Label::new("a"))
+///     .descendant(Label::new("b"))
+///     .any_child()
+///     .child(Label::new("c"));
+///
+/// // Can also use `builder.build()`.
+/// let query: JsonPathQuery = builder.into();
+///
+/// assert_eq!(format!("{query}"), "$['a']..['b'][*]['c']");
+/// ```
 pub struct JsonPathQueryBuilder {
     nodes: Vec<NodeTemplate>,
 }
 
 impl JsonPathQueryBuilder {
+    /// Initialize an empty builder.
+    /// 
+    /// # Examples
+    /// ```
+    /// # use rsonpath_lib::query::{JsonPathQuery, JsonPathQueryNode, Label, builder::JsonPathQueryBuilder};
+    /// let builder = JsonPathQueryBuilder::new();
+    /// let query: JsonPathQuery = builder.into();
+    ///
+    /// assert_eq!(query.root(), JsonPathQueryNode::Root(None));
+    /// ```
+    #[must_use]
+    #[inline(always)]
     pub fn new() -> Self {
         Self { nodes: vec![] }
     }
 
+    /// Add a child selector with a given label.
+    #[must_use]
+    #[inline(always)]
     pub fn child(mut self, label: Label) -> Self {
         self.nodes.push(NodeTemplate::Child(label));
         self
     }
 
+    /// Add a wildcard child selector.
+    #[must_use]
+    #[inline(always)]
     pub fn any_child(mut self) -> Self {
         self.nodes.push(NodeTemplate::AnyChild);
         self
     }
 
+    /// Add a descendant selector with a given label.
+    #[must_use]
+    #[inline(always)]
     pub fn descendant(mut self, label: Label) -> Self {
         self.nodes.push(NodeTemplate::Descendant(label));
         self
     }
 
+    /// Consume the builder and produce a [`JsonPathQuery`].
+    #[must_use]
+    #[inline]
     pub fn build(self) -> JsonPathQuery {
         let mut last = None;
 
@@ -61,7 +85,15 @@ impl JsonPathQueryBuilder {
     }
 }
 
+impl Default for JsonPathQueryBuilder {
+    #[inline(always)]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl From<JsonPathQueryBuilder> for JsonPathQuery {
+    #[inline(always)]
     fn from(value: JsonPathQueryBuilder) -> Self {
         value.build()
     }
