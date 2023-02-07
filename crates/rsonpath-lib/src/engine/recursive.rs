518fde744cd7b059bf265e6bd7bf3b3eabfd2bfb
Mateusz Gienieczko <mat@gienieczko.com>
2023-02-07 03:23:22 +0000

    First fix to the recursive engine.

--- crates/rsonpath-lib/src/engine/recursive.rs
+++ crates/rsonpath-lib/src/engine/recursive.rs
@@ -149,9 +149,22 @@ where
         let fallback_state = self.automaton[state].fallback_state();
         let is_fallback_accepting = self.automaton.is_accepting(fallback_state);
         let is_list = self.bytes[open_idx] == b'[';
+        let needs_commas = is_list && is_fallback_accepting;
+        let needs_colons = !is_list && self.automaton.has_transition_to_accepting(state);
 
-        if is_list && is_fallback_accepting {
+        if needs_commas {
             self.classifier.turn_commas_on(open_idx);
+        } else {
+            self.classifier.turn_commas_off();
+        }
+
+        if needs_colons {
+            self.classifier.turn_colons_on(open_idx);
+        } else {
+            self.classifier.turn_colons_off();
+        }
+
+        if needs_commas {
             next_event = self.classifier.next();
             if let Some(Structural::Closing(close_idx)) = next_event {
                 for idx in (open_idx + 1)..close_idx {
@@ -174,23 +187,132 @@ where
             }
             debug!("Event: {next_event:?}");
             match next_event {
+                Some(Structural::Comma(idx)) => {
+                    latest_idx = idx;
+                    next_event = self.classifier.next();
+                    let is_next_opening = matches!(next_event, Some(Structural::Opening(_)));
+
+                    if !is_next_opening && is_list && is_fallback_accepting {
+                        debug!("Accepting on comma.");
+                        self.result.report(idx);
+                    }
+                    next_event = None;
+                }
+                Some(Structural::Colon(idx)) => {
+                    debug!(
+                        "Colon, label ending with {:?}",
+                        std::str::from_utf8(&self.bytes[(if idx < 8 { 0 } else { idx - 8 })..idx])
+                            .unwrap_or("[invalid utf8]")
+                    );
+
+                    latest_idx = idx;
+                    next_event = self.classifier.next();
+                    let is_next_opening = matches!(next_event, Some(Structural::Opening(_)));
+
+                    if !is_next_opening {
+                        let mut any_matched = false;
+
+                        for &(label, target) in self.automaton[state].transitions() {
+                            if self.automaton.is_accepting(target) && self.is_match(idx, label)? {
+                                debug!("Accept {idx}");
+                                self.result.report(idx);
+                                any_matched = true;
+                                break;
+                            }
+                        }
+                        let fallback_state = self.automaton[state].fallback_state();
+                        if !any_matched && self.automaton.is_accepting(fallback_state) {
+                            debug!("Value accepted by fallback.");
+                            self.result.report(idx);
+                        }
+                        /*#[cfg(feature = "unique-labels")]
+                        {
+                            let is_next_closing =
+                                matches!(self.next_event, Some(Structural::Closing(_)));
+                            if any_matched
+                                && !is_next_closing
+                                && self.automaton.is_unitary(self.state)
+                            {
+                                let opening = if self.is_list { b'[' } else { b'{' };
+                                debug!("Skipping unique state from {}", opening as char);
+                                let stop_at = classifier.skip(opening);
+                                self.next_event = Some(Structural::Closing(stop_at));
+                            }
+                        }*/
+                    }
+                }
                 Some(Structural::Opening(idx)) => {
                     debug!("Opening, falling back");
                     increase_depth!(self);
 
-                    #[cfg(feature = "tail-skip")]
-                    if self.automaton.is_rejecting(fallback_state) {
-                        latest_idx = self.classifier.skip(self.bytes[idx]);
-                    } else {
-                        latest_idx = self.run(fallback_state, idx)?;
-                    }
-                    #[cfg(not(feature = "tail-skip"))]
-                    {
-                        latest_idx = self.run(fallback_state, idx)?;
+                    let mut matched = None;
+                    let colon_idx = {
+                        let mut colon_idx = idx - 1;
+                        while colon_idx > 0 && self.bytes[colon_idx].is_ascii_whitespace() {
+                            colon_idx -= 1;
+                        }
+                        (self.bytes[colon_idx] == b':').then_some(colon_idx)
+                    };
+
+                    if let Some(colon_idx) = colon_idx {
+                        debug!(
+                            "Colon backtracked, label ending with {:?}",
+                            std::str::from_utf8(
+                                &self.bytes
+                                    [(if colon_idx < 8 { 0 } else { colon_idx - 8 })..colon_idx]
+                            )
+                            .unwrap_or("[invalid utf8]")
+                        );
+                        for &(label, target) in self.automaton[state].transitions() {
+                            if self.is_match(colon_idx, label)? {
+                                matched = Some(target);
+                                if self.automaton.is_accepting(target) {
+                                    debug!("Accept {idx}");
+                                    self.result.report(colon_idx);
+                                }
+                                break;
+                            }
+                        }
                     }
 
-                    if is_list && is_fallback_accepting {
-                        self.classifier.turn_commas_on(latest_idx);
+                    let end_idx = match matched {
+                        Some(target) => {
+                            self.run(target, idx)?
+                        }
+                        None => {
+                            let fallback = self.automaton[state].fallback_state();
+                            debug!("Falling back to {fallback}");
+
+                            if self.automaton.is_accepting(fallback) {
+                                debug!("Accept {idx}");
+                                self.result.report(idx);
+                            }
+
+                            #[cfg(feature = "tail-skip")]
+                            if self.automaton.is_rejecting(fallback_state) {
+                                self.classifier.skip(self.bytes[idx])
+                            } else {
+                                self.run(fallback_state, idx)?
+                            }
+                            #[cfg(not(feature = "tail-skip"))]
+                            {
+                                self.run(fallback_state, idx)?
+                            }
+                        }
+                    };
+
+                    latest_idx = end_idx;
+
+                    if needs_commas {
+                        self.classifier.turn_commas_on(end_idx);
+                    } else {
+                        self.classifier.turn_commas_off();
+                    }
+
+                    if needs_colons {
+                        self.classifier.turn_colons_on(end_idx);
+                    } else {
+                        self.classifier.turn_colons_off();
                     }
 
                     next_event = None;
@@ -199,51 +321,7 @@ where
                     debug!("Closing, popping stack");
                     latest_idx = idx;
                     decrease_depth!(self);
-                    self.classifier.turn_commas_off();
                     break;
-                }
-                Some(Structural::Comma(idx)) => {
-                    latest_idx = idx;
-                    if is_list && is_fallback_accepting {
-                        debug!("Accepting on comma.");
-                        self.result.report(idx);
-                    }
-                    next_event = None;
-                }
-                Some(Structural::Colon(idx)) => {
-                    latest_idx = idx;
-                    next_event = self.classifier.next();
-                    let next_opening = match next_event {
-                        Some(Structural::Opening(idx)) => Some(idx),
-                        _ => None,
-                    };
-                    let mut any_matched = false;
-                    for &(label, target) in self.automaton[state].transitions() {
-                        let is_accepting = self.automaton.is_accepting(target);
-                        if let Some(next_idx) = next_opening {
-                            if self.is_match(idx, label)? {
-                                debug!("Matched transition to {target}");
-                                if is_accepting {
-                                    self.result.report(idx);
-                                }
-                                increase_depth!(self);
-                                latest_idx = self.run(target, next_idx)?;
-                                next_event = None;
-                                any_matched = true;
-                                break;
-                            }
-                        } else if is_accepting && self.is_match(idx, label)? {
-                            debug!("Matched transition to acceptance in {target}");
-                            self.result.report(idx);
-                            any_matched = true;
-                            break;
-                        }
-                    }
-
-                    if !any_matched && is_fallback_accepting {
-                        debug!("Value accepted by fallback.");
-                        self.result.report(idx);
-                    }
                 }
                 None => break,
             }
