2f3b8319d2daf41122d18fbd7caf00e262d19110
David Zwerdling <zwerdlds@gmail.com>
2023-04-03 10:14:06 -0700

    nix shell pinned package versions to nixpkgs repo

--- shell.nix
+++ shell.nix
@@ -1,30 +1,41 @@
-{ pkgs ? import <nixpkgs> { }, ... }:
-with pkgs;
+{ nixpkgs ? import <nixpkgs> { }, ... }:
 let
-  unstable = import <nixos-unstable> {};
-in
-mkShell rec {
-  buildInputs = with pkgs; [
-    rustup
-    gcc
-    pkg-config
-    lldb
-    cargo
-    rustc
-    clippy
-    openssl
-    freetype
-    expat
-    unstable.just
-    llvmPackages.bintools
-    git-cliff
-    git-lfs
-    rust-analyzer
-  ];
+  stable = import (nixpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "884e3b68be02ff9d61a042bc9bd9dd2a358f95da";
+    sha256 = "ISWz16oGxBhF7wqAxefMPwFag6SlsA9up8muV79V9ck=";
+  }) {};
+  unstable = import (nixpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "53dad94e874c9586e71decf82d972dfb640ef044";
+    sha256 = "9FNIqrxDZgSliGGN2XJJSvcDYmQbgOANaZA4UWnTdg4=";
+  }) {};
+in stable.mkShell rec {
+  buildInputs =
+    (with stable; [
+      rustup
+      gcc
+      pkg-config
+      lldb
+      cargo
+      rustc
+      clippy
+      openssl
+      freetype
+      expat
+      llvmPackages.bintools
+      git-lfs
+      git-cliff
+      rust-analyzer
+    ]) ++ (with unstable; [
+      just
+    ]);
 
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  RUST_SRC_PATH = "${nixpkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   shellHook = ''
     export PATH="$CARGO_HOME:$PATH";
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib.makeLibraryPath buildInputs}";
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${nixpkgs.lib.makeLibraryPath buildInputs}";
   '';
 }
