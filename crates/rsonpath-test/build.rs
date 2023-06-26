bce3edab79daf2ace8a7ecfa3e5cfcb4f3632bb7
Mateusz Gienieczko <mat@gienieczko.com>
2023-06-26 01:22:48 +0100

    rewrote all tests

--- crates/rsonpath-test/build.rs
+++ crates/rsonpath-test/build.rs
@@ -1,11 +1,15 @@
-use std::fs;
-use std::process::Command;
+use std::{fs, io, path::Path};
+use std::{
+    io::{BufRead, BufReader, ErrorKind},
+    process::Command,
+};
 
 use eyre::WrapErr;
 
 fn main() -> eyre::Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=./tests/documents/toml/*");
+    println!("cargo:rerun-if-changed=./tests/documents/json/large/*");
     println!("cargo:rerun-if-changed=./tests/end_to_end.rs");
     println!("cargo:rerun-if-changed=../rsonpath-test-codegen/**/*");
 
@@ -17,7 +21,16 @@ fn main() -> eyre::Result<()> {
         .wrap_err("error generating end-to-end tests")?;
     let source = format!("{}", tokens);
 
-    fs::write(OUTPUT_FILE_PATH, source).wrap_err("error writing to test file")?;
+    let new_md5 = md5::compute(&source);
+    let old_comment = read_md5_from_comment(OUTPUT_FILE_PATH)?;
+    let new_comment = format!("// {:x}\n", new_md5);
+
+    if old_comment.is_some_and(|x| x == new_comment) {
+        eprintln!("MD5 digest up to date, not regenerating");
+        return Ok(());
+    }
+    let contents = new_comment + &source;
+    fs::write(OUTPUT_FILE_PATH, contents).wrap_err("error writing to test file")?;
 
     let rustfmt_status = Command::new("rustfmt").arg(OUTPUT_FILE_PATH).status()?;
 
@@ -29,4 +42,19 @@ fn main() -> eyre::Result<()> {
     );
 
     Ok(())
+}
+
+fn read_md5_from_comment<P: AsRef<Path>>(path: P) -> Result<Option<String>, io::Error> {
+    match fs::File::open(&path) {
+        Ok(f) => {
+            let mut buffer = BufReader::new(f);
+            let mut first_line = String::new();
+
+            buffer.read_line(&mut first_line)?;
+
+            Ok(Some(first_line))
+        }
+        Err(err) if err.kind() == ErrorKind::NotFound => Ok(None),
+        Err(err) => Err(err),
+    }
 }
