f30ac43abdd3322d07e92388ed7ca351a35c5682
Mateusz Gienieczko <mat@gienieczko.com>
2024-06-28 16:16:35 +0200

    chore(test): fix tests in quotes that were erroneously disabled

--- crates/rsonpath-lib/src/classification/quotes/avx2_32.rs
+++ crates/rsonpath-lib/src/classification/quotes/avx2_32.rs
@@ -31,9 +31,9 @@ impl BlockAvx2Classifier {
     }
 }
 
-#[cfg(all(test, cfg = "avx_32"))]
+#[cfg(test)]
 mod tests {
-    use super::Avx2QuoteClassifier32;
+    use super::{Constructor, QuotesImpl};
     use crate::{
         input::{Input, OwnedBytes},
         result::empty::EmptyRecorder,
@@ -41,18 +41,38 @@ mod tests {
     };
     use test_case::test_case;
 
-    #[test_case("" => None)]
-    #[test_case("abcd" => Some(0))]
-    #[test_case(r#""abcd""# => Some(0b01_1111))]
-    #[test_case(r#""num": 42, "string": "something" "# => Some(0b0_0111_1111_1110_0011_1111_1000_0000_1111))]
-    #[test_case(r#"abc\"abc\""# => Some(0b00_0000_0000))]
-    #[test_case(r#"abc\\"abc\\""# => Some(0b0111_1110_0000))]
-    #[test_case(r#"{"aaa":[{},{"b":{"c":[1,2,3]}}],"# => Some(0b0000_0000_0000_0110_0011_0000_0001_1110))]
-    fn single_block(str: &str) -> Option<u32> {
+    #[test_case("", 0)]
+    #[test_case("abcd", 0)]
+    #[test_case(r#""abcd""#, 0b01_1111)]
+    #[test_case(r#""num": 42, "string": "something" "#, 0b0_0111_1111_1110_0011_1111_1000_0000_1111)]
+    #[test_case(r#"abc\"abc\""#, 0b00_0000_0000)]
+    #[test_case(r#"abc\\"abc\\""#, 0b0111_1110_0000)]
+    #[test_case(r#"{"aaa":[{},{"b":{"c":[1,2,3]}}],"#, 0b0000_0000_0000_0110_0011_0000_0001_1110)]
+    fn single_block(str: &str, expected: u32) {
+        if !std::arch::is_x86_feature_detected!("avx2") {
+            return;
+        }
+
         let owned_str = str.to_owned();
-        let input = OwnedBytes::new(&owned_str).unwrap();
+        let input = OwnedBytes::from(owned_str);
+        let mut leading_padding = input.leading_padding_len() as u32;
         let iter = input.iter_blocks::<_, 32>(&EmptyRecorder);
-        let mut classifier = Avx2QuoteClassifier32::new(iter);
-        classifier.next().unwrap().map(|x| x.within_quotes_mask)
+        let mut classifier = Constructor::new(iter);
+
+        // Drop padding-only blocks.
+        while leading_padding >= 32 {
+            let mask = classifier.next().unwrap().unwrap().within_quotes_mask;
+            assert_eq!(mask, 0);
+            leading_padding -= 32;
+        }
+
+        // The actual classification is now either contained in the next block,
+        // or split between the two next blocks. Combine them.
+        let first_mask = classifier.next().unwrap().unwrap().within_quotes_mask;
+        let len_in_first_mask = if leading_padding == 0 { 0 } else { 32 - leading_padding };
+        let second_mask = classifier.next().unwrap().unwrap().within_quotes_mask;
+        let combined_mask = (first_mask >> leading_padding) | (second_mask << len_in_first_mask);
+
+        assert_eq!(combined_mask, expected);
     }
 }
