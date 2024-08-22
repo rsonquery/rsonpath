b477595e921ce8710bb6b13b582fa07889ceff47
Mateusz Gienieczko <mat@gienieczko.com>
2024-08-22 19:43:25 +0200

    add simd_dispatch to the example

--- crates/rsonpath-lib/src/ricardo.rs
+++ crates/rsonpath-lib/src/ricardo.rs
@@ -6,53 +6,54 @@ use crate::{
         simd::Simd,
         structural::{BracketType, Structural, StructuralIterator},
     },
-    input::{self, Input},
+    input::{self, error, Input},
     result::empty::EmptyRecorder,
     FallibleIterator,
 };
 
 #[inline]
 pub fn run(file: &File) -> Result<(), Box<dyn Error>> {
+    // SAFETY: We keep the file open throughout the entire duration.
     let input = unsafe { input::MmapInput::map_file(file)? };
     let simd_c = classification::simd::configure();
 
     classification::simd::config_simd!(simd_c => |simd| {
-            let iter = input.iter_blocks(&EmptyRecorder);
-            let quote_classifier = simd.classify_quoted_sequences(iter);
-            let mut structural_classifier = simd.classify_structural_characters(quote_classifier);
-            structural_classifier.turn_colons_and_commas_off();
-
-            while let Some(event) = structural_classifier.next()? {
-                match event {
-                    Structural::Closing(b, idx) => {
-                        match b {
-                            BracketType::Square => println!("] at index {idx}"),
-                            BracketType::Curly => println!("}} at index {idx}"),
-                        }
-                    },
-                    Structural::Opening(b, idx) =>{
-                        match b {
-                            BracketType::Square => println!("[ at index {idx}"),
-                            BracketType::Curly => println!("{{ at index {idx}"),
-                        }
-                    },
-                    Structural::Colon(_) => unreachable!(),
-                    Structural::Comma(_) => unreachable!(),
-                }
-            }
-
-            Ok::<_, input::error::InputError>(())
-        })?;
+        classification::simd::dispatch_simd!(simd; input, simd => fn<I, V>(
+            input: I,
+            simd: V,
+        ) -> Result<(), error::InputError> where
+        I: Input,
+        V: Simd,{
+                run_impl::<I, V>(&input, simd)
+            })
+    })?;
 
     Ok(())
 }
 
-/*
+#[inline(always)]
+fn run_impl<I, V>(input: &I, simd: V) -> Result<(), error::InputError>
+where
+    I: Input,
+    V: Simd,
+{
+    let iter = input.iter_blocks::<_, 64>(&EmptyRecorder);
+    let quote_classifier = simd.classify_quoted_sequences(iter);
+    let mut structural_classifier = simd.classify_structural_characters(quote_classifier);
+    structural_classifier.turn_colons_and_commas_off();
 
-        classification::simd::dispatch_simd!(simd; input => fn<I, V>(input: I) -> Result<(), Box<dyn Error>>
-        where
-            I: input::Input,
-            V: Simd {
-                Ok(())
-            })
-*/
+    while let Some(event) = structural_classifier.next()? {
+        match event {
+            Structural::Closing(b, idx) => match b {
+                BracketType::Square => println!("] at index {idx}"),
+                BracketType::Curly => println!("}} at index {idx}"),
+            },
+            Structural::Opening(b, idx) => match b {
+                BracketType::Square => println!("[ at index {idx}"),
+                BracketType::Curly => println!("{{ at index {idx}"),
+            },
+            Structural::Colon(_) | Structural::Comma(_) => unreachable!(),
+        }
+    }
+    Ok(())
+}
