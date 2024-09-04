ca0068fa311d78b3a53018c4e93905e9bc658780
Ricardo Kraft <kraftricardo97@gmail.com>
2024-09-04 15:30:48 +0200

    Added estimation function

--- crates/rsonpath-lib/src/lut_main.rs
+++ crates/rsonpath-lib/src/lut_main.rs
@@ -5,6 +5,8 @@ use rsonpath::lookup_table::{lut_perfect_hashing, lut_performance, util};
 /// Main function that processes command-line arguments, validates paths,
 /// and runs performance tests.
 fn main() -> Result<(), Box<dyn Error>> {
+    calc_max_digits();
+
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() == 2 {
@@ -94,4 +96,32 @@ fn print_bad_input_error_msg() {
     "
     );
     std::process::exit(1);
+}
+
+// Assume every single character is a '{' character. This is of course an upper bound calculation then.
+fn calc_max_digits() {
+    // We iterate from 1 to 64 bits
+    for bit in 1..=64 {
+        // Calculate the maximum JSON size based on the number of bits
+        let max_json_size = (1_u64 << bit) * 8;
+
+        // Determine the size in appropriate units
+        let size_in_kb = max_json_size as f64 / 1024.0;
+        let size_in_mb = size_in_kb / 1024.0;
+        let size_in_gb = size_in_mb / 1024.0;
+        let size_in_tb = size_in_gb / 1024.0;
+
+        // Print the size in different units based on the magnitude
+        println!(
+            "Bit: {}, Max JSON size: {} B, {:.2} kB, {:.2} MB, {:.2} GB, {:.2} TB",
+            bit, max_json_size, size_in_kb, size_in_mb, size_in_gb, size_in_tb
+        );
+    }
+
+    // Idea: At 43 bits the input json is 64TB
+    // 1) what if instead of usize we use u_43_bits since the other bits will always be 0
+    // 2) 64 - 43 = 21 . We have 21 bits where we could save more structural data per key.
+    //      - maybe the usage counter? This way we know which values to cache, because high counter means often skipped?
+    //      - the deepness, but I am not sure what value this brings
+    // 3) When the input JSON is < 32GB we can even use 32bit per key
 }
