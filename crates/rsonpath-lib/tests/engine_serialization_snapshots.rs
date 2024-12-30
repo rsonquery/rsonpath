1fddf02f8e7b8a20d595f12881e9d09c58a58a29
Mateusz Gienieczko <mat@gienieczko.com>
2024-12-30 03:02:07 +0100

    test: added snapshot tests for the serde feature
    Added snapshot tests based on `insta` to both the rsonpath-syntax
    and rsonpath-lib serialization features.
    
    Ref: #624

---
+++ crates/rsonpath-lib/tests/engine_serialization_snapshots.rs
@@ -0,0 +1,43 @@
+use insta::assert_ron_snapshot;
+use rsonpath::engine::{Compiler, RsonpathEngine};
+
+mod ron {
+    use super::*;
+    use std::error::Error;
+
+    fn engine(string: &str) -> Result<RsonpathEngine, Box<dyn Error>> {
+        let query = rsonpath_syntax::parse(string)?;
+        let engine = RsonpathEngine::compile_query(&query)?;
+        Ok(engine)
+    }
+
+    #[test]
+    fn empty_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&engine("$")?);
+        Ok(())
+    }
+
+    #[test]
+    fn readme_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&engine("$.jsonpath[*]")?);
+        Ok(())
+    }
+
+    #[test]
+    fn jsonpath_example_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&engine("$..phoneNumbers[*].number")?);
+        Ok(())
+    }
+
+    #[test]
+    fn real_life_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&engine("$.personal.details.contact.information.phones.home")?);
+        Ok(())
+    }
+
+    #[test]
+    fn slice() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&engine("$..entries[3:5:7]")?);
+        Ok(())
+    }
+}
---
+++ crates/rsonpath-syntax/tests/query_serialization_snapshots.rs
@@ -0,0 +1,55 @@
+use insta::assert_ron_snapshot;
+use rsonpath_syntax::parse;
+
+mod ron {
+    use super::*;
+    use std::error::Error;
+
+    #[test]
+    fn empty_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse("$")?);
+        Ok(())
+    }
+
+    #[test]
+    fn readme_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse("$.jsonpath[*]")?);
+        Ok(())
+    }
+
+    #[test]
+    fn jsonpath_example_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse("$..phoneNumbers[*].number")?);
+        Ok(())
+    }
+
+    #[test]
+    fn real_life_query() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse("$.personal.details.contact.information.phones.home")?);
+        Ok(())
+    }
+
+    #[test]
+    fn slice() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse("$..entries[3:5:7]")?);
+        Ok(())
+    }
+
+    #[test]
+    fn multiple_selectors() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse("$..entries['abc', 4, 7:10:13]")?);
+        Ok(())
+    }
+
+    #[test]
+    fn filter() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse("$..user[?@.id == 'value']..entities..url")?);
+        Ok(())
+    }
+
+    #[test]
+    fn nested_filter() -> Result<(), Box<dyn Error>> {
+        assert_ron_snapshot!(&parse(r#"$.a[?@.b == "abc" && $[*][?@.c > 3.13]]"#)?);
+        Ok(())
+    }
+}
