93f83a9ac4650fc445eea9c30f65c88f9b273166
Thibault Tisserand <gzordrai@proton.me>
2026-05-08 18:19:07 +0200

    feat: add tests for force_input flag

--- crates/rsonpath/tests/cli_tests.rs
+++ crates/rsonpath/tests/cli_tests.rs
@@ -2,6 +2,7 @@
 fn cli_tests() {
     trycmd::TestCases::new()
         .default_bin_name("rq")
+        .env("RUST_BACKTRACE", "0")
         .case("tests/cmd/*.toml")
         .case("../../README.md")
         .case("../../book/src/**/*.md")
